1ed8b6c42019bcbde491790e5fd48ce9a99bcacf
LarissaGiovanaAlaminos <lari.alaminos@gmail.com>
2025-04-16 21:08:08 -0300

    Create func consultCoupon

--- src/main/java/br/alaminos/api/service/CouponService.java
+++ src/main/java/br/alaminos/api/service/CouponService.java
@@ -2,29 +2,39 @@ package br.alaminos.api.service;
 
 import br.alaminos.api.domain.coupon.Coupon;
 import br.alaminos.api.domain.coupon.dto.CouponRequestDTO;
+import br.alaminos.api.domain.event.Event;
 import br.alaminos.api.repositories.CouponRepository;
+import br.alaminos.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class CouponService {
 
     @Autowired
-    CouponRepository repository;
+    private CouponRepository repository;
 
     @Autowired
-    EventService eventService;
+    private EventRepository eventRepository;
 
-    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO dto){
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
 
-        Coupon newCoupon = new Coupon();
-        newCoupon.setCode(dto.code());
-        newCoupon.setDiscount(dto.discount());
-        newCoupon.setValid(dto.valid());
-        newCoupon.setEvent(eventService.getEvent(eventId));
+        Coupon coupon = new Coupon();
+        coupon.setCode(couponData.code());
+        coupon.setDiscount(couponData.discount());
+        coupon.setValid(new Date(couponData.valid()));
+        coupon.setEvent(event);
 
-        return repository.save(newCoupon);
+        return repository.save(coupon);
+    }
+
+    public List<Coupon> consultCoupon(UUID id, Date date) {
+        return repository.findByEventIdAndValidAfter(id, date);
     }
 }
